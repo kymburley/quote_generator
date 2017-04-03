aed097f6f60182567eeb0d3d0347d3ba4389a554
CakedOnMakeup <kimberly.mozzetti@gmail.com>
2017-04-03 18:47:22 +0200

    Added Get Quote Button functionality

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-   var url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
-   console.log(url);
+   $(".quoteBtn").click(function() {
+      var url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+
+      $.getJSON(url, function(data) {
+         var quote = data.quoteText;
+         var author = data.quoteAuthor;
+
+         $(".theQuote").html(quote);
+         $(".theAuthor").html(author);
+console.log(data);
+      });      
+   });
 });
