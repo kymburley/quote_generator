4315c8999fcd0306f205b1f622ec34d27d4746b7
CakedOnMakeup <kimberly.mozzetti@gmail.com>
2017-04-04 08:21:32 +0200

    fix: Cross origin problem
    Switched from getJSON to ajax to handle the cross-origin header
    error I got when I deployed to github pages.

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
    $(".quoteBtn").click(function() {
-      var url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+      $.ajax({
+         url: "http://api.forismatic.com/api/1.0/",
+         jsonp: "jsonp",
+         dataType: "jsonp",
+         data: {
+            method: "getQuote",
+            lang: "en",
+            format: "jsonp"
+         },
+         success: function(data) {
+            console.log(data);
+            $(".theQuote").html(data.quoteText);
+            $(".theAuthor").html(data.quoteAuthor);
+         },
+         error: function(errorMsg) {
+            console.log(errorMsg);
+         }
+      })
 
-      $.getJSON(url, function(data) {
-         var quote = data.quoteText;
-         var author = data.quoteAuthor;
-
-         $(".theQuote").html(quote);
-         $(".theAuthor").html(author);
-console.log(data);
-      });      
    });
+
 });
